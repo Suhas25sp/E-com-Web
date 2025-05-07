7fe01652bc7ceba58f7d7ce6ef1abe3930b155e6
Suhas <Suhaspatil132500@gmail.com>
2025-05-07 19:13:02 +0530

    push

---
+++ script.js
@@ -0,0 +1,15 @@
+const bar = document.getElementById("bar");
+const cls = document.getElementById("close");
+const nav = document.getElementById("navbar");
+
+if (bar) {
+  bar.addEventListener("click", () => {
+    nav.classList.add("active");
+  });
+}
+
+if (cls) {
+  cls.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
+}
